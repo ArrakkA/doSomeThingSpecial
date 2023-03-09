a49f1b5f84ef2fed432ec1f6b4f876f9a19027db
DESKTOP-S2C7CJG\computer <kys@greenit.cc>
2023-03-09 09:53:19 +0900

    package 네임 변경

--- src/main/java/gms/something/special/SpecialApplication.java
+++ src/main/java/gms/something/special/SpecialApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package gms.something.special;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/gms/something/special/SpecialApplicationTests.java
+++ src/test/java/gms/something/special/SpecialApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package gms.something.special;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
